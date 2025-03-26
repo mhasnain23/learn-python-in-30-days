10538a260b89f7923fab77438cbd8cadeb31239a
mhasnain23 <tryfrelancer23@gmail.com>
2025-03-26 21:21:45 +0500

    updated

--- chapter 6/lists.py
+++ chapter 6/lists.py
@@ -39,3 +39,30 @@ print(numbers)  # Output: [1, 1, 3, 4, 5, 9]
 numbers = [4, 2, 9, 1]
 numbers.sort(reverse=True)
 print(numbers)  # Output: [9, 4, 2, 1]
+
+words = ["apple", "kiwi", "banana"]
+words.sort(key=len)
+print(words)  # Output: ['kiwi', 'apple', 'banana']
+
+
+words = ["apple", "kiwi", "banana"]
+words.sort(key=lambda word: word[-1])
+print(words)  # Output: ['banana', 'apple', 'kiwi']  # Sorted by last letter
+
+numbers = [1, 2, 5, 7, 10]
+numbers.reverse()
+print(numbers)  # Output: [10, 7, 5, 2, 1]
+
+
+for name in dir(str):
+    if name.startswith("__"):
+        continue
+    print(name)
+
+# List comprehension
+# Without if condition
+squared: list = [x**2 for x in [1, 2, 3, 4, 5] ] #  if x > 3 place this if condition and see
+print(squared, " : ", type(squared))  # Output: [1, 4, 9, 16, 25]
+
+squared: list = [x**2 for x in [1, 2, 3, 4, 5] if x > 3] #  if x > 3 place this if condition and see
+print(squared, " : ", type(squared))  # Output: [1, 4, 9, 16, 25]
